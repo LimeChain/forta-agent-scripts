62084d2fbefdf6fceda8cf4de49d252ca6b82c1d
dikel <dikelito@tutamail.com>
2021-10-14 13:35:35 +0300

    Update Swap Enabled agent

--- balancer-suite/swap-enabled/src/agent.spec.js
+++ balancer-suite/swap-enabled/src/agent.spec.js
@@ -9,6 +9,7 @@ const { provideHandleTransaction } = require("./agent")
 
 const vaultAddress = "0xBA12222222228d8Ba445958a75a0704d566BF2C8"
 const poolAddress = "0x1111"
+const fromAddress = "0xffff"
 const eventTopic = "0x5a9e84f78f7957cb4ed7478eb0fcad35ee4ecbe2e0f298420b28a3955392573f"
 const dataTrue = "0x0000000000000000000000000000000000000000000000000000000000000001"
 const dataFalse = "0x0000000000000000000000000000000000000000000000000000000000000000"
@@ -28,7 +29,7 @@ const logsNoMatchEvent = {
   topics: [ '0x0' ],
 }
 
-describe("pause state change agent", () => {
+describe("swap enabled agent", () => {
   let handleTransaction
 
   const mockContract = {
@@ -37,10 +38,13 @@ describe("pause state change agent", () => {
 
   const mockCreateContract = () => mockContract
 
-  const createTxEvent = ({ logs, addresses }) =>
+  const createTxEvent = ({ logs, addresses, from }) =>
     createTransactionEvent({
       receipt: { logs },
-      addresses
+      addresses,
+      transaction: {
+        from
+      }
     })
 
   beforeAll(() => {
@@ -48,10 +52,11 @@ describe("pause state change agent", () => {
   })
 
   describe("handleTransaction", () => {
-    it("returns empty findings if no PausedStateChanged event", async () => {
+    it("returns empty findings if no SwapEnabledSet event", async () => {
       const txEvent = createTxEvent({
         logs: [logsNoMatchEvent],
-        addresses: { [poolAddress]: true }
+        addresses: { [poolAddress]: true },
+        from: fromAddress
       })
 
       const findings = await handleTransaction(txEvent)
@@ -62,7 +67,8 @@ describe("pause state change agent", () => {
     it("returns a finding on SwapEnabledSet event with state 'true'", async () => {
       const txEvent = createTxEvent({
         logs: [logsMatchEventEnabled],
-        addresses: { [poolAddress]: true, [vaultAddress]: true }
+        addresses: { [poolAddress]: true, [vaultAddress]: true },
+        from: fromAddress
       })
 
       mockContract.getVault.mockReturnValueOnce(vaultAddress)
@@ -73,11 +79,13 @@ describe("pause state change agent", () => {
           name: "Balancer Pool Swap Enabled Changed",
           description: `Swaps for pool ${poolAddress} are enabled`,
           alertId: "BALANCER-SWAP-ENABLED-CHANGED",
+          protocol: "balancer",
           type: FindingType.Suspicious,
           severity: FindingSeverity.Medium,
           metadata: {
             address: poolAddress,
             swapEnabled: true,
+            from: fromAddress
           },
         }),
       ])
@@ -85,7 +93,8 @@ describe("pause state change agent", () => {
     it("returns a finding on SwapEnabledSet event with state 'false'", async () => {
       const txEvent = createTxEvent({
         logs: [logsMatchEventDisabled],
-        addresses: { [poolAddress]: true, [vaultAddress]: true }
+        addresses: { [poolAddress]: true, [vaultAddress]: true },
+        from: fromAddress
       })
 
       mockContract.getVault.mockReturnValueOnce(vaultAddress)
@@ -96,11 +105,13 @@ describe("pause state change agent", () => {
           name: "Balancer Pool Swap Enabled Changed",
           description: `Swaps for pool ${poolAddress} are disabled`,
           alertId: "BALANCER-SWAP-ENABLED-CHANGED",
+          protocol: "balancer",
           type: FindingType.Suspicious,
           severity: FindingSeverity.Medium,
           metadata: {
             address: poolAddress,
             swapEnabled: false,
+            from: fromAddress
           },
         }),
       ])
