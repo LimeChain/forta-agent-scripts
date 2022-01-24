52b17a33bb95cc16e2bf0930034cd836a4485c26
dikel <dikelito@tutamail.com>
2022-01-24 16:00:59 +0200

    create borrow-amount-close-to-total-supply agent

---
+++ iron-bank-suite/borrow-amount-close-to-total-supply/src/agent.spec.js
@@ -0,0 +1,82 @@
+const {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  ethers
+} = require("forta-agent")
+const { provideHandleInitialize, handleTransaction } = require("./agent")
+
+const market = "0x41c84c0e2ee0b740cf0d31f63f3b6f627dc6b393"
+const borrower = "0xborrower"
+
+// The user borrows 11 of the underlying asset.
+// The tatalSupply of the underlying asset is
+// totalSupply * exchangeRateStored = 100
+const borrowAmount = ethers.utils.parseEther("11")
+const totalSupply = ethers.utils.parseEther("1")
+const exchangeRateStored = ethers.utils.parseEther("100")
+
+describe("borrow-amount-close-to-total-supply agent", () => {
+  const mockTxEvent = { filterLog: jest.fn() }
+  const mockContract = {
+    totalSupply: jest.fn(),
+    exchangeRateStored: jest.fn()
+  }
+  const markets = {
+    "0x41c84c0e2ee0b740cf0d31f63f3b6f627dc6b393": { name: "cyWETH", decimals: 18 }
+  }
+
+  const mockCreateContract = () => mockContract
+  const mockGetMarkets = () => markets
+
+  beforeAll(async () => {
+    initialize = provideHandleInitialize(mockCreateContract, mockGetMarkets)
+    await initialize()
+  })
+
+  beforeEach(() => {
+    mockContract.totalSupply.mockReset()
+    mockContract.exchangeRateStored.mockReset()
+    mockTxEvent.filterLog.mockReset()
+  })
+
+  describe("handleTransaction", () => {
+    it("returns empty findings if there isn't a borrow with amount close to totalSupply", async () => {
+      mockTxEvent.filterLog.mockReturnValue([])
+      const findings = await handleTransaction(mockTxEvent)
+
+      expect(findings).toStrictEqual([])
+    });
+
+    it("returns a finding if there is a borrow with amount close to totalSupply", async () => {      
+      const mockMintEvent = {
+        address: market,
+        args: { borrower, borrowAmount },
+      }
+      mockTxEvent.filterLog.mockReturnValueOnce([mockMintEvent])
+      mockContract.totalSupply.mockReturnValueOnce(totalSupply)
+      mockContract.exchangeRateStored.mockReturnValueOnce(exchangeRateStored)
+
+      const findings = await handleTransaction(mockTxEvent)
+
+      expect(findings).toStrictEqual([
+        Finding.fromObject({
+          name: "Borrow amount close to total supply",
+          description: `${borrower} borrowed 11.00 from ` + 
+          `cyWETH which is close to the total supply ` + 
+          `(100.00) of the market`,
+          alertId: "BORROW-AMOUNT-CLOSE-TO-TOTAL-SUPPLY",
+          protocol: "iron-bank",
+          severity: FindingSeverity.Medium,
+          type: FindingType.Info,
+          metadata: {
+            borrower,
+            market,
+            borrowAmount: "11.0",
+            percentage: "11.00"
+          },
+        }),
+      ])
+    })
+  })
+})
---
+++ iron-bank-suite/borrow-amount-close-to-total-supply/src/iron-bank-markets.js
@@ -0,0 +1,112 @@
+module.exports = {
+    markets: {
+        "0x41c84c0e2ee0b740cf0d31f63f3b6f627dc6b393": {
+            name: "cyWETH",
+            decimals: 18
+        },
+        "0x8e595470ed749b85c6f7669de83eae304c2ec68f": {
+            name: "cyDAI",
+            decimals: 18
+        },
+        "0xe7bff2da8a2f619c2586fb83938fa56ce803aa16": {
+            name: "cyLINK",
+            decimals: 18
+        },
+        "0xfa3472f7319477c9bfecdd66e4b948569e7621b9": {
+            name: "cyYFI",
+            decimals: 18
+        },
+        "0x12a9cc33a980daa74e00cc2d1a0e74c57a93d12c": {
+            name: "cySNX",
+            decimals: 18
+        },
+        "0x8fc8bfd80d6a9f17fb98a373023d72531792b431": {
+            name: "cyWBTC",
+            decimals: 8
+        },
+        "0x48759f220ed983db51fa7a8c0d2aab8f3ce4166a": {
+            name: "cyUSDT",
+            decimals: 6
+        },
+        "0x76eb2fe28b36b3ee97f3adae0c69606eedb2a37c": {
+            name: "cyUSDC",
+            decimals: 6
+        },
+        "0xa7c4054afd3dbbbf5bfe80f41862b89ea05c9806": {
+            name: "cySUSD",
+            decimals: 18
+        },
+        "0xbe86e8918dfc7d3cb10d295fc220f941a1470c5c": {
+            name: "cyMUSD",
+            decimals: 18
+        },
+        "0xa8caea564811af0e92b1e044f3edd18fa9a73e4f": {
+            name: "cyEURS",
+            decimals: 2
+        },
+        "0xca55f9c4e77f7b8524178583b0f7c798de17fd54": {
+            name: "cySEUR",
+            decimals: 18
+        },
+        "0x7736ffb07104c0c400bb0cc9a7c228452a732992": {
+            name: "cyDPI",
+            decimals: 18
+        },
+        "0xbddeb563e90f6cbf168a7cda4927806477e5b6c6": {
+            name: "cyUSDP",
+            decimals: 18
+        },
+        "0xfeeb92386a055e2ef7c2b598c872a4047a7db59f": {
+            name: "cyUNI",
+            decimals: 18
+        },
+        "0x226f3738238932ba0db2319a8117d9555446102f": {
+            name: "cySUSHI",
+            decimals: 18
+        },
+        "0x00e5c0774a5f065c285068170b20393925c84bf3": {
+            name: "cyEUR",
+            decimals: 18
+        },
+        "0x3c9f5385c288ce438ed55620938a4b967c080101": {
+            name: "cyKRW",
+            decimals: 18
+        },
+        "0x215f34af6557a6598dbda9aa11cc556f5ae264b1": {
+            name: "0x215f34af6557a6598dbda9aa11cc556f5ae264b1",
+            decimals: 18
+        },
+        "0x86bbd9ac8b9b44c95ffc6baae58e25033b7548aa": {
+            name: "cyAUD",
+            decimals: 18
+        },
+        "0xecab2c76f1a8359a06fab5fa0ceea51280a97ecf": {
+            name: "cyGBP",
+            decimals: 18
+        },
+        "0x1b3e95e8ecf7a7cab6c4de1b344f94865abd12d5": {
+            name: "cyCHF",
+            decimals: 18
+        },
+        "0xb8c5af54bbdcc61453144cf472a9276ae36109f9": {
+            name: "cyCRV",
+            decimals: 18
+        },
+        "0x9d029cd7cedcb194e2c361948f279f1788135bb2": {
+            name: "cyCREAM",
+            decimals: 18
+        },
+        "0x30190a3b52b5ab1daf70d46d72536f5171f22340": {
+            name: "cyAAVE",
+            decimals: 18
+        },
+        "0x9e8e207083ffd5bdc3d99a1f32d1e6250869c1a9": {
+            name: "cyMIM",
+            decimals: 18
+        },
+        "0x672473908587b10e65dab177dbaeadcbb30bf40b": {
+            name: "cyZAR",
+            decimals: 18
+        },
+    }
+}
