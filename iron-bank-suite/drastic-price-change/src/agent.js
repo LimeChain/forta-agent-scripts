31783e2cd9823d3cf435377e320203739ccbba31
dikel <dikelito@tutamail.com>
2022-02-02 16:51:10 +0200

    create drastic-price-change agent

---
+++ iron-bank-suite/drastic-price-change/src/agent.js
@@ -0,0 +1,116 @@
+const { Finding, FindingSeverity, FindingType, getEthersProvider, ethers } = require("forta-agent")
+const { Contract, Provider } = require('ethers-multicall')
+const { getMarkets } = require("../../helper")
+
+const INTERVAL = 1 * 60 * 60 // 1 hour
+
+// Check if the delta of the prices is between 90% of the interval and the interval
+const MAX_DELTA_INTERVAL = INTERVAL * 90 / 100 
+const PERCENTAGE_THRESHOLD = 30
+
+const oracleAddress = "0x6b96c414ce762578c3e7930da9114cffc88704cb"
+const abi = ["function getUnderlyingPrice(address cToken) public view returns (uint)"]
+
+const oraclePrices = {}
+
+let markets
+let marketsAddresses
+let ethcallProvider
+const oracleContract = new Contract(oracleAddress, abi)
+
+function provideInitialize(getMarkets, createProvider) {
+  return async function initialize() {
+    markets = await getMarkets()
+    marketsAddresses = Object.keys(markets)
+    marketsAddresses.forEach(a => oraclePrices[a] = [])
+
+    ethcallProvider = createProvider()
+  }
+}
+
+async function handleBlock(blockEvent) {
+  const findings = []
+
+  // Check prices every 10 blocks (~2 minutes)
+  if (blockEvent.blockNumber % 10 !== 0) return findings
+
+  const timestamp = blockEvent.block.timestamp
+
+  const calls = marketsAddresses.map(a => oracleContract.getUnderlyingPrice(a))
+  const prices = await ethcallProvider.all(calls)
+
+  Object.entries(markets).forEach(([address, market], i) => {
+    let decimals = markets[address].decimalsUnderlying
+
+    // getUnderlying always returns the price with 18 decimals and we have
+    // to convert it to the decimals of the underlying asset
+    // for cyWETH it is 18; for cyBTC is 18 + 10 = 28
+    decimals = 18 + (18 - decimals)
+    const price = ethers.utils.formatUnits(prices[i], decimals)
+
+    const oldPrice = addPrice(address, price, timestamp)
+
+    if (!oldPrice) return
+
+    const percentage = calculatePercentage(price, oldPrice)
+    if (Math.abs(percentage) > PERCENTAGE_THRESHOLD) {
+      findings.push(createAlert(market.name, price, oldPrice, percentage))
+    }
+  })
+
+  return findings
+}
+
+function addPrice(market, price, timestamp) {
+  // Append price
+  oraclePrices[market].push({
+    price,
+    timestamp,
+  })
+
+  // Filter out any prices that fall outside of the time interval
+  oraclePrices[market] = oraclePrices[market].filter(
+    (p) => p.timestamp >= timestamp - INTERVAL
+  )
+
+  // Return null if there is only one price (the current one)
+  if (oraclePrices[market].length === 1) return null
+
+  // Return null if the difference between the current and the oldest price 
+  // is not at least 90% of the timeInterval
+  const oldestPrice = oraclePrices[market][0]
+  if (timestamp - oldestPrice.timestamp < MAX_DELTA_INTERVAL) return null
+
+  return oldestPrice.price
+}
+
+const createAlert = (market, price, oldPrice, percentage) => {
+  return Finding.fromObject({
+    name: "Drastic price change",
+    description: `Price for ${market} changed with ${percentage}% for the last ${INTERVAL/60} minutes`,
+    alertId: "IRON-BANK-DRASTIC-PRICE-CHANGE",
+    protocol: "iron-bank",
+    severity: FindingSeverity.Medium,
+    type: FindingType.Info,
+    metadata: {
+      market,
+      oldPrice,
+      price,
+      percentage
+    },
+  })
+}
+
+createProvider = () => {
+  return new Provider(getEthersProvider(), 1)
+}
+
+const calculatePercentage = (current, previous) => {
+  return ((current / previous - 1) * 100).toFixed(2)
+}
+
+module.exports = {
+  initialize: provideInitialize(getMarkets, createProvider),
+  provideInitialize,
+  handleBlock
+}
---
+++ iron-bank-suite/drastic-price-change/src/agent.spec.js
@@ -0,0 +1,85 @@
+const {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  ethers
+} = require("forta-agent")
+const { provideInitialize, handleBlock } = require("./agent")
+
+const market = "0x41c84c0e2ee0b740cf0d31f63f3b6f627dc6b393"
+
+// Create 2 prices; the difference is 100%
+const oraclePrice1 = ethers.utils.parseEther("100")
+const oraclePrice2 = ethers.utils.parseEther("200")
+
+const mockBlockEvent1 = {
+  blockNumber: 10,
+  block: {
+    timestamp: 100
+  },
+}
+
+const mockBlockEvent2 = {
+  blockNumber: 20,
+  block: {
+    // Set the block timestamp to almost 1 hour after the first
+    timestamp: 100 + (60 * 60) - 10
+  },
+}
+
+describe("drastic-price-change agent", () => {
+  const markets = {}
+  markets[market] = { 
+    name: "cyWETH",
+    decimalsUnderlying: 18
+  }
+  const mockGetMarkets = () => markets
+  
+  const mockProvider = { all: jest.fn() }
+  const mockCreateProvider = () => mockProvider
+
+  beforeAll(() => {
+    const initialize = provideInitialize(mockGetMarkets, mockCreateProvider)
+    initialize()
+  })
+
+  beforeEach(() => {
+    mockProvider.all.mockReset()
+  })
+
+  describe("handleTransaction", () => {
+    it("returns empty findings if there isn't a drastic price change", async () => {
+      mockProvider.all.mockReturnValueOnce([oraclePrice1])
+      const findings = await handleBlock(mockBlockEvent1)
+
+      expect(findings).toStrictEqual([])
+    });
+
+    it("returns a finding if there is a drastic price change", async () => {
+
+      mockProvider.all.mockReturnValueOnce([oraclePrice1])
+      mockProvider.all.mockReturnValueOnce([oraclePrice2])
+
+
+      await handleBlock(mockBlockEvent1)
+      const findings = await handleBlock(mockBlockEvent2)
+
+      expect(findings).toStrictEqual([
+        Finding.fromObject({
+          name: "Drastic price change",
+          description: `Price for cyWETH changed with 100.00% for the last 60 minutes`,
+          alertId: "IRON-BANK-DRASTIC-PRICE-CHANGE",
+          protocol: "iron-bank",
+          severity: FindingSeverity.Medium,
+          type: FindingType.Info,
+          metadata: {
+            market: "cyWETH",
+            oldPrice: "100.0",
+            price: "200.0",
+            percentage: "100.00"
+          },
+        }),
+      ])
+    })
+  })
+})
