03cb549ac6cd671efd3d1f67ace5d968197d6ee4
Tomislav Drachev <drachev.td@gmail.com>
2022-04-04 17:16:33 +0300

    Updated agent to Typescript

---
+++ iron-bank-suite/high-gas-ts/src/agent.spec.js
@@ -0,0 +1,74 @@
+const {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  createTransactionEvent,
+} = require("forta-agent");
+
+const { provideHandleTransaction, provideInitialize } = require("./agent.ts");
+
+const market = "0x41c84c0e2ee0b740cf0d31f63f3b6f627dc6b393";
+
+describe("high-gas agent", () => {
+  let handleTransaction;
+  const markets = {};
+  markets[market] = {
+    name: "cyWETH",
+    decimalsUnderlying: 18,
+  };
+  const mockGetMarkets = () => markets;
+
+  const mockGetTxReceipt = jest.fn();
+
+  beforeAll(async () => {
+    const initialize = provideInitialize(mockGetMarkets);
+    await initialize();
+    handleTransaction = provideHandleTransaction(mockGetTxReceipt);
+  });
+
+  const createTxEvent = ({ addresses }) =>
+    createTransactionEvent({
+      transaction: { hash: "0x0" },
+
+      addresses,
+    });
+
+  describe("handleTransaction", () => {
+    it("returns empty findings if there isn't a high gas usage", async () => {
+      const gasUsedDecimal = 100_000;
+      const gasUsed = `0x${gasUsedDecimal.toString(16)}`;
+      mockGetTxReceipt.mockResolvedValueOnce({ gasUsed });
+      const txEvent = createTxEvent({
+        addresses: { [market]: true },
+      });
+
+      const findings = await handleTransaction(txEvent);
+
+      expect(findings).toStrictEqual([]);
+    });
+
+    it("returns a finding if there is a high gas usage", async () => {
+      const gasUsedDecimal = 4_000_000;
+      const gasUsed = `0x${gasUsedDecimal.toString(16)}`;
+      mockGetTxReceipt.mockResolvedValueOnce({ gasUsed });
+      const txEvent = createTxEvent({
+        addresses: { [market]: true },
+      });
+      const findings = await handleTransaction(txEvent);
+
+      expect(findings).toStrictEqual([
+        Finding.fromObject({
+          name: "High gas",
+          description: `Gas used is > 3M`,
+          alertId: "IRON-BANK-HIGH-GAS",
+          protocol: "iron-bank",
+          severity: FindingSeverity.Medium,
+          type: FindingType.Info,
+          metadata: {
+            gasUsed: gasUsedDecimal.toString(),
+          },
+        }),
+      ]);
+    });
+  });
+});
---
+++ iron-bank-suite/high-gas-ts/src/helper.ts
@@ -0,0 +1,62 @@
+import { Interface } from "@ethersproject/abi";
+
+const { ethers, getEthersProvider } = require("forta-agent");
+const { Contract, Provider } = require("ethers-multicall");
+
+const controllerAddress: string = "0xab1c342c7bf5ec5f02adea1c2270670bca144cbb";
+const controllerAbi: string[] = [
+  "function getAllMarkets() public view returns (address[] memory)",
+];
+
+const ethcallProvider = new Provider(getEthersProvider(), 1);
+
+module.exports = {
+  getMarkets: async (): Promise<any> => {
+    const tokenAbi: string[] = [
+      "function symbol() external view returns (string memory)",
+      "function underlying() public view returns (address)",
+      "function decimals() external view returns (uint8)",
+    ];
+
+    const contract = new ethers.Contract(
+      controllerAddress,
+      controllerAbi,
+      getEthersProvider()
+    );
+
+    const marketsAddresses: string[] = (await contract.getAllMarkets()).map(
+      (address: string) => address.toLowerCase()
+    );
+
+    let calls = marketsAddresses.map((address: string) => {
+      const tokenContract = new Contract(address, tokenAbi);
+      return tokenContract.symbol();
+    });
+
+    const symbols = await ethcallProvider.all(calls);
+
+    calls = marketsAddresses.map((address: string) => {
+      const tokenContract = new Contract(address, tokenAbi);
+      return tokenContract.underlying();
+    });
+
+    const underlyingAddress = await ethcallProvider.all(calls);
+
+    calls = underlyingAddress.map((address: string) => {
+      const tokenContract = new Contract(address, tokenAbi);
+      return tokenContract.decimals();
+    });
+
+    const decimals = await ethcallProvider.all(calls);
+    const markets: { [key: string]: any } = {};
+    marketsAddresses.forEach((market: string, i: number) => {
+      markets[market] = { name: symbols[i], decimalsUnderlying: decimals[i] };
+    });
+
+    return markets;
+  },
+
+  getProvider: () => {
+    return ethcallProvider;
+  },
+};
