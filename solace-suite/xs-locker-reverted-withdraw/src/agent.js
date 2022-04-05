6403464494d725261af2c5d147cc38178fe08174
Tomislav Drachev <drachev.td@gmail.com>
2022-04-05 10:46:34 +0300

    Refactored unit test for 100% coverage and refactored agent.js to use getTransaction receipt

--- solace-suite/xs-locker-reverted-withdraw/src/agent.js
+++ solace-suite/xs-locker-reverted-withdraw/src/agent.js
@@ -1,47 +1,61 @@
-const { Finding, FindingSeverity, FindingType } = require("forta-agent")
+const {
+  Finding,
+  FindingSeverity,
+  FindingType,
+  getTransactionReceipt,
+} = require("forta-agent");
 
-const xsLockerAddress = "0x501ace47c5b0c2099c4464f681c3fa2ecd3146c1"
+const xsLockerAddress = "0x501ace47c5b0c2099c4464f681c3fa2ecd3146c1";
 
 const withdrawMethodsSignatures = [
   "0x00f714ce", // withdraw
   "0xd6186627", // withdrawInPart
   "0x8d50b33d", // withdrawMany
-]
+];
 
-const handleTransaction = async (txEvent) => {
-  const findings = []
+function provideHandleTransaction(getTxReceipt) {
+  return async function handleTransaction(txEvent) {
+    const findings = [];
 
-  const { from, to, gas, data } = txEvent.transaction
-  const { status, gasUsed } = txEvent.receipt
+    const { from, to, gas, data } = txEvent.transaction;
 
-  // Return if the transaction is not send to the xsLocker contract
-  if (to !== xsLockerAddress) return findings
+    // Return if the transaction is not send to the xsLocker contract
+    if (to !== xsLockerAddress) return findings;
 
-  // Return if the called method is not a withdraw
-  const isWithdraw = withdrawMethodsSignatures.some(e => data.startsWith(e))
-  if (!isWithdraw) return findings
+    // Return if the called method is not a withdraw
+    const isWithdraw = withdrawMethodsSignatures.some((e) =>
+      data.startsWith(e)
+    );
 
-  // Return if the transaction is successful
-  if (status) return findings
+    if (!isWithdraw) return findings;
 
-  // Return if the reason for the revert is out of gas
-  if (gas === gasUsed) return findings
+    const { status, gasUsed } = await getTxReceipt(txEvent.hash);
+    await console.log("Status: " + status + " " + "GasUsed: " + gasUsed);
+    // Return if the transaction is successful
+    if (status) return findings;
 
-  findings.push(Finding.fromObject({
-    name: "xsLocker attempt to withdraw from a locked lock",
-    description: `Address ${from} tried to withdraw from a locked lock`,
-    alertId: "SOLACE-XS-LOCKER-REVERTED-WITHDRAW",
-    protocol: "solace",
-    severity: FindingSeverity.Medium,
-    type: FindingType.Info,
-    metadata: {
-      from
-    },
-  }))
+    // Return if the reason for the revert is out of gas
+    if (gas === gasUsed) return findings;
 
-  return findings;
+    findings.push(
+      Finding.fromObject({
+        name: "xsLocker attempt to withdraw from a locked lock",
+        description: `Address ${from} tried to withdraw from a locked lock`,
+        alertId: "SOLACE-XS-LOCKER-REVERTED-WITHDRAW",
+        protocol: "solace",
+        severity: FindingSeverity.Medium,
+        type: FindingType.Info,
+        metadata: {
+          from,
+        },
+      })
+    );
+
+    return findings;
+  };
 }
 
 module.exports = {
-  handleTransaction,
+  handleTransaction: provideHandleTransaction(getTransactionReceipt),
+  provideHandleTransaction: provideHandleTransaction,
 };
--- solace-suite/xs-locker-reverted-withdraw/src/agent.spec.js
+++ solace-suite/xs-locker-reverted-withdraw/src/agent.spec.js
@@ -1,49 +1,74 @@
-const {
-  FindingType,
-  FindingSeverity,
-  Finding,
-} = require("forta-agent")
-const { handleTransaction } = require("./agent")
+const { FindingType, FindingSeverity, Finding } = require("forta-agent");
+const { provideHandleTransaction } = require("./agent");
 
-const fromAddress = "0xsomeaddress"
-const xsLockerAddress = "0x501ace47c5b0c2099c4464f681c3fa2ecd3146c1"
-const withdrawMethodSignature = "0x00f714ce"
+const fromAddress = "0xsomeaddress";
+const xsLockerAddress = "0x501ace47c5b0c2099c4464f681c3fa2ecd3146c1";
+const withdrawMethodSignature = "0x00f714ce";
 
 const txEventWithoutWithdraw = {
+  hash: "0x0",
   transaction: {
     from: fromAddress,
     to: xsLockerAddress,
     gas: "0x100",
-    data: "0xsomedata"
+    data: "0xsomedata",
   },
-  receipt: {
-    status: true,
-    gasUsed: "0x99"
-  }
-}
+};
 
 const txEventWithWithdraw = {
+  hash: "0x0",
   transaction: {
     from: fromAddress,
     to: xsLockerAddress,
     gas: "0x100",
     data: withdrawMethodSignature,
   },
-  receipt: {
-    status: false,
-    gasUsed: "0x99"
-  }
-}
+};
+
+const txEventWithoutXsLockerAddress = {
+  hash: "0x0",
+  transaction: {
+    from: fromAddress,
+    to: "0x2",
+    gas: "0x100",
+    data: withdrawMethodSignature,
+  },
+};
 
 describe("xsLocker reverted withdraw", () => {
   describe("handleTransaction", () => {
+    let handleTransaction;
+    const mockTxReceipt = jest.fn();
+
+    beforeAll(() => {
+      handleTransaction = provideHandleTransaction(mockTxReceipt);
+    });
+
+    it("returns empty findings if 'to' is not to xsLockerAddress", async () => {
+      const findings = await handleTransaction(txEventWithoutXsLockerAddress);
+      expect(findings).toStrictEqual([]);
+    });
+
+    it("returns empty findings if status is successful", async () => {
+      mockTxReceipt.mockResolvedValueOnce({ status: true, gasUsed: "0x99" });
+      const findings = await handleTransaction(txEventWithWithdraw);
+      expect(findings).toStrictEqual([]);
+    });
+
+    it("returns empty findings if tx ran out of gas (gas == gasUsed)", async () => {
+      mockTxReceipt.mockResolvedValueOnce({ status: false, gasUsed: "0x100" });
+      const findings = await handleTransaction(txEventWithWithdraw);
+      expect(findings).toStrictEqual([]);
+    });
+
     it("returns empty findings there is no withdraw from a locked lock", async () => {
-      const findings = await handleTransaction(txEventWithoutWithdraw)
-      expect(findings).toStrictEqual([])
-    })
+      const findings = await handleTransaction(txEventWithoutWithdraw);
+      expect(findings).toStrictEqual([]);
+    });
 
     it("returns a finding if there is an attempt to withdraw from a locked lock", async () => {
-      const findings = await handleTransaction(txEventWithWithdraw)
+      mockTxReceipt.mockResolvedValue({ status: false, gasUsed: "0x99" });
+      const findings = await handleTransaction(txEventWithWithdraw);
       expect(findings).toStrictEqual([
         Finding.fromObject({
           name: "xsLocker attempt to withdraw from a locked lock",
@@ -53,10 +78,10 @@ describe("xsLocker reverted withdraw", () => {
           severity: FindingSeverity.Medium,
           type: FindingType.Info,
           metadata: {
-            from: fromAddress
+            from: fromAddress,
           },
         }),
-      ])
-    })
-  })
-})
+      ]);
+    });
+  });
+});
